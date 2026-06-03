1d2c2ff5a78ecf0595b988c3b9f9255d605785f2
Hamdi <hamdi.abdwd@gmail.com>
2026-06-03 22:23:35 +0200

    Add ads.txt for AdSense

--- src/config/adsense.ts
+++ src/config/adsense.ts
@@ -1,5 +1,5 @@
 export const ADSENSE_CONFIG = {
-  publisherId: 'ca-pub-1234567890123456', // Placeholder Publisher ID, to be swapped by user
+  publisherId: 'ca-pub-5384145624117299', // Placeholder Publisher ID, to be swapped by user
   slots: {
     leaderboard: '9876543210',    // Hero section bottom / top of page banner
     inGrid: '8765432109',         // Inline bento grid banner
