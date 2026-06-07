a5973fb76540e7259b545cf66499db3ec2f17df6
Hamdi <hamdi.abdwd@gmail.com>
2026-06-07 19:10:51 +0200

    fix: AdSense meta tag in root layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,11 @@
 import './globals.css';
 
+export const metadata = {
+  other: {
+    'google-adsense-account': 'ca-pub-5384145624117299',
+  },
+};
+
 export default function RootLayout({
   children,
 }: {
